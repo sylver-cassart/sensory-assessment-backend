ef13112766400d0b3989f1a89ac72f8d841c38c3
Sylver <112153530+sylver-cassart@users.noreply.github.com>
2025-09-23 07:17:11 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./server/routes.js";
+import { registerRoutes } from "./server/routes";
 
 const app = express();
 
